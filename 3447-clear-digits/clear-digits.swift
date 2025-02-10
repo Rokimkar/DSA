cbe58bc3a172870a59b14e3a56106bf7cb918d03
Sanchit Kumar Singh <sanchitkumarsingh@gmail.com>
2025-02-11 01:53:34 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (44.44%) - LeetSync

--- 3447-clear-digits/clear-digits.swift
+++ 3447-clear-digits/clear-digits.swift
@@ -1,13 +1,5 @@
 class Solution {
     func clearDigits(_ s: String) -> String {
-        String(Array(s).reduce(into: [Character]()) { 
-            if Int(String($1)) == nil {
-                $0.append($1)
-            } else {
-                if !$0.isEmpty {
-                    $0.removeLast()
-                }
-            }
-        })
+        String(Array(s).reduce(into: [Character]()) { Int(String($1)) == nil ? $0.append($1) : (_ = $0.removeLast()) })
     }
 }
