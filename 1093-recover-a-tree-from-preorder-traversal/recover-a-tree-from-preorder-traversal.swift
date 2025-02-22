f540168df1859afdaf414d44b6dbd351de5d4b68
Sanchit Kumar Singh <sanchitkumarsingh@gmail.com>
2025-02-22 22:51:27 +0530

    Time: 9 ms (0.00%) | Memory: 20.6 MB (0.00%) - LeetSync

---
+++ 1093-recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.swift
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func recoverFromPreorder(_ traversal: String) -> TreeNode? {
+        var nodeStack = [TreeNode]()
+        let traversalChars = Array(traversal)
+        var i = 0
+        var dashCount = 0
+        while i < traversalChars.count {
+            if traversalChars[i] == "-" {
+                dashCount += 1
+                i += 1
+            } else {
+                var j = i
+                while j < traversalChars.count && traversalChars[j] != "-" {
+                    j += 1
+                }
+                let num = String(traversalChars[i..<j])
+                i = j
+                let node = TreeNode(Int(num) ?? 0)
+                if dashCount < nodeStack.count {
+                    while dashCount != nodeStack.count {
+                        nodeStack.removeLast()
+                    }
+                    nodeStack.last!.right = node
+                    nodeStack.append(node)
+                } else {
+                    if !nodeStack.isEmpty {
+                        nodeStack.last!.left = node
+                    }
+                    nodeStack.append(node)
+                }
+                dashCount = 0
+            }
+        }
+        return nodeStack.first!
+    }
+}
