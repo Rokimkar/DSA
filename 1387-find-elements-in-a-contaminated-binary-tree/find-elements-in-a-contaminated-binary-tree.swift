4b1c4a46303b330ebc3d5bc45ca3e14b0b1e717e
Sanchit Kumar Singh <sanchitkumarsingh@gmail.com>
2025-02-23 01:55:06 +0530

    Time: 44 ms (9.29%) | Memory: 22.1 MB (6.43%) - LeetSync

---
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.swift
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+import Collections
+
+class FindElements {
+    var nodeValues = [Int]()
+
+    init(_ root: TreeNode?) {
+        if let root = root {
+            // using level order traversal to initialise the nodeValues array
+            var nodeDeque = Deque<TreeNode>()
+            root.val = 0
+            nodeDeque.append(root)
+            nodeValues.append(root.val)
+            while let node = nodeDeque.popFirst() {
+                if node.left != nil {
+                    node.left!.val = 2*(node.val) + 1
+                    nodeValues.append(node.left!.val)
+                    nodeDeque.append(node.left!)
+                }
+                if node.right != nil {
+                    node.right!.val = 2*(node.val) + 2
+                    nodeValues.append(node.right!.val)
+                    nodeDeque.append(node.right!)
+                }
+            }
+        }
+    }
+    
+    func find(_ target: Int) -> Bool {
+        return nodeValues.contains(target)
+    }
+}
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * let obj = FindElements(root)
+ * let ret_1: Bool = obj.find(target)
+ */
